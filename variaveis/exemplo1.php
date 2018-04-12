38a93f3b72703dcae51c193ad97d026cb7bb0cb4
Bruno HC <brunohc1991@gmail.com>
2018-04-11 21:17:39 -0300

    criando a pasta variaveis

---
+++ variaveis/exemplo1.php
@@ -0,0 +1,19 @@
+<?php
+
+//Basicos
+$nome = "HCode";
+$site = "www.hcode.com.br";
+$ano = 1990;
+$salario = 5500.99;
+$bloqueado = false;
+
+
+//Compostos
+$frutas = array("abacaxi","Laranja","morango");
+$nascimento = new datetime();
+
+//Especiais
+$arquivos = fopen("exemplo1.php","r");
+
+var_dump($arquivos);
+?>
