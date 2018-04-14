7918511c80211237ac026daba76337e9517551d4
Bruno HC <brunohc1991@gmail.com>
2018-04-14 00:37:11 -0300

    teste

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -1,5 +1,24 @@
 <?php
 
-
+class usuario{
+    private $idUsuario;
+    private $desLogin;
+    private $desSenha;
+    private $dtCadastro;
+    
+    public function getIdUsuario(){
+        return $this->idUsuario;
+    }
+    public function getDesLogin(){
+        return $this->desLogin;
+    }
+    public function getDesSenha(){
+        return $this->desSenha;
+    }
+    public function getDtCadastro(){
+        return $this->dtCadastro;
+    }
+    
+}
 
 ?>
